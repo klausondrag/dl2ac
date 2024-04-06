23746fb521b4e3d1bf79ee47980873a36ceb66a1
Klaus Ondrag <klaus.ondrag@outlook.com>
2024-04-07 07:10:52 +0800

    Add testing of labels

--- dl2ac/config.py
+++ dl2ac/config.py
@@ -6,16 +6,19 @@ from loguru import logger
 LABEL_START = 'dl2ac'
 
 # 'dl2ac.is-authelia': true
-AUTHELIA_KEY = rf'{LABEL_START}\.is-authelia'
-AUTHELIA_VALUE = 'true'
+IS_AUTHELIA_KEY = f'{LABEL_START}.is-authelia'
+IS_AUTHELIA_VALUE = 'true'
 
-logger.info(f'{AUTHELIA_KEY=}')
-logger.info(f'{AUTHELIA_VALUE=}')
+logger.info(f'{IS_AUTHELIA_KEY=}')
+logger.info(f'{IS_AUTHELIA_VALUE=}')
 
-LABEL_RULES_START = rf'{LABEL_START}\.rules\.(.+)'
+LABEL_RULES_START_REGEX = rf'{LABEL_START}\.rules\.(.+)'
+LABEL_RULES_START_FORMAT = f'{LABEL_START}.rules.{{rule_name}}'
 
 # 'dl2ac.rules.one.policy': 'one_factor'
-POLICY_KEY_REGEX = re.compile(rf'{LABEL_RULES_START}\.policy')
+POLICY_KEY_REGEX = re.compile(rf'{LABEL_RULES_START_REGEX}\.policy')
+POLICY_KEY_FORMAT = f'{LABEL_RULES_START_FORMAT}.policy'
 
 # 'dl2ac.rules.one.priority': '20'
-PRIORITY_KEY_REGEX = re.compile(rf'{LABEL_RULES_START}\.priority')
+PRIORITY_KEY_REGEX = re.compile(rf'{LABEL_RULES_START_REGEX}\.priority')
+PRIORITY_KEY_FORMAT = f'{LABEL_RULES_START_FORMAT}.priority'
---
+++ tests/models_test.py
@@ -0,0 +1,39 @@
+from hypothesis import given, strategies as st
+
+from dl2ac import config, models
+
+rule_name_strategy = st.text(
+    alphabet=st.characters(exclude_characters=['.', '\n']), min_size=1
+)
+
+
+@given(st.booleans())
+def test_is_authelia_label(is_authelia: bool) -> None:
+    label_key = config.IS_AUTHELIA_KEY
+    label_value = str(is_authelia).lower()
+    label = models.IsAutheliaLabel.try_parse(label_key, label_value)
+    assert (label is not None) == is_authelia
+
+
+@given(
+    rule_name_strategy,
+    st.sampled_from(models.AutheliaPolicy),
+)
+def test_policy_label(rule_name: str, policy: models.AutheliaPolicy) -> None:
+    print(policy)
+    label_key = config.POLICY_KEY_FORMAT.format(rule_name=rule_name)
+    label_value = policy.value
+    label = models.PolicyLabel.try_parse(label_key, label_value)
+    assert label is not None
+    assert label.rule_name == rule_name
+    assert label.policy == policy
+
+
+@given(rule_name_strategy, st.integers())
+def test_priority_label(rule_name: str, priority: int) -> None:
+    label_key = config.PRIORITY_KEY_FORMAT.format(rule_name=rule_name)
+    label_value = str(priority)
+    label = models.PriorityLabel.try_parse(label_key, label_value)
+    assert label is not None
+    assert label.rule_name == rule_name
+    assert label.priority == priority
